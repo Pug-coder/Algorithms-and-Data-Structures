c66fb718c4aca3a1cbeaa60ba20aa59a19118597
pug-coder <andrey.suchanov@yandex.ru>
2023-01-05 19:00:22 +0300

    create_graph added and refactoring

--- FormalLanguagesTheory/lab5/mfa.py
+++ FormalLanguagesTheory/lab5/mfa.py
@@ -15,26 +15,26 @@ class MFA:
 
         # here add what we parsed
         for dtype, dec in declarations:
-            if dtype == 'node':
-                node_id = dec['id']
+            if dtype == "node":
+                node_id = dec["id"]
                 self.nodes.add(node_id)
 
-                if dec['label']:
-                    self.lables[dec['id']] = dec['label']
+                if dec["label"]:
+                    self.lables[dec["id"]] = dec["label"]
 
-                for flag in dec['flags']:
-                    if flag == 'init_flag':
+                for flag in dec["flags"]:
+                    if flag == "init_flag":
                         self.inits.add(node_id)
-                    elif flag == 'final_flag':
+                    elif flag == "final_flag":
                         self.finals.add(node_id)
                     else:
-                        raise Exception(f'unknown flag {flag}')
+                        raise Exception(f"unknown flag {flag}")
 
-            elif dtype == 'edge':
-                node_id1 = dec['from']
-                node_id2 = dec['to']
-                symbol = dec['symbol']
-                memory = dec['memory']
+            elif dtype == "edge":
+                node_id1 = dec["from"]
+                node_id2 = dec["to"]
+                symbol = dec["symbol"]
+                memory = dec["memory"]
 
                 # check equality length
                 if memory_len == -1:
@@ -46,14 +46,14 @@ class MFA:
                 if symbol.isdigit():
                     assert int(symbol) <= memory_len and int(symbol) > 0
                 else:
-                    assert symbol == 'eps' or symbol == 'any' or len(symbol) == 1
+                    assert (
+                        symbol == "eps" or symbol == "any" or len(symbol) == 1
+                    )
 
                 self.nodes.add(node_id1)
                 self.nodes.add(node_id2)
 
-                self.edges.add(
-                    (node_id1, node_id2, symbol, tuple(memory))
-                )
+                self.edges.add((node_id1, node_id2, symbol, tuple(memory)))
 
         # start conditions size must be one
         assert len(self.inits) == 1
@@ -68,27 +68,34 @@ class MFA:
         self.numbering = dict([(x, i) for (i, x) in enumerate(self.nodes)])
         self.rev_numbering = dict([(i, x) for (i, x) in enumerate(self.nodes)])
         network = nx.MultiDiGraph()
-        [network.add_edge(self.numbering[x[0]], self.numbering[x[1]]) for x in self.edges]
+        [
+            network.add_edge(self.numbering[x[0]], self.numbering[x[1]])
+            for x in self.edges
+        ]
         connection_num = nx.all_pairs_node_connectivity(network)
         connection = {}
         for key, value in connection_num.items():
             nums = set(filter(lambda x: value[x] == 1, value.keys()))
             nums.add(key)
-            connection[self.rev_numbering[key]] = set(map(lambda x: self.rev_numbering[x], nums))
+            connection[self.rev_numbering[key]] = set(
+                map(lambda x: self.rev_numbering[x], nums)
+            )
         self.traps = set()
         for n in self.nodes:
             if not self.finals & connection[n]:
                 self.traps.add(n)
 
     def set_alphabeth(self):
-        self.alphabeth = set(filter(
-            lambda x: x != 'eps' and x != 'any' and not x.isdigit(),
-            map(lambda x: x[2], self.edges)
-        ))
+        self.alphabeth = set(
+            filter(
+                lambda x: x != "eps" and x != "any" and not x.isdigit(),
+                map(lambda x: x[2], self.edges),
+            )
+        )
 
     def normalize_edges(self):
         # replace all "any" for transition set by every symbol
-        any_edges = set(filter(lambda x: x[2] == 'any', self.edges))
+        any_edges = set(filter(lambda x: x[2] == "any", self.edges))
         for ae in any_edges:
             c = list(copy(ae))
             for alpha in self.alphabeth:
@@ -106,7 +113,9 @@ class MFA:
             for transit in transits:
                 groups[transit[1]].add(transit[2:])
 
-            to_pop = [key if not value else None for key, value in groups.items()]
+            to_pop = [
+                key if not value else None for key, value in groups.items()
+            ]
             [groups.pop(key) if key else None for key in to_pop]
 
             # for every distanation
@@ -115,31 +124,50 @@ class MFA:
                 # print(node, '->', destination,)
                 # print(groups[destination])
 
-                flags_to_alpha = dict([(y, set()) for _, y in groups[destination]])
+                flags_to_alpha = dict(
+                    [(y, set()) for _, y in groups[destination]]
+                )
                 [flags_to_alpha[y].add(x) for x, y in groups[destination]]
-                print('F2A', flags_to_alpha)
+                print("F2A", flags_to_alpha)
                 for key_combination, alphas_set in flags_to_alpha.items():
                     # print(key_combination, alphas_set, self.alphabeth)
                     if alphas_set == self.alphabeth:
                         # print('CAN COLLECT', key_combination)
-                        s = set(map(lambda alpha: (node, destination, alpha, key_combination), self.alphabeth))
+                        s = set(
+                            map(
+                                lambda alpha: (
+                                    node,
+                                    destination,
+                                    alpha,
+                                    key_combination,
+                                ),
+                                self.alphabeth,
+                            )
+                        )
                         print(s)
                         self.edges = self.edges.difference(s)
-                        self.edges.add((node, destination, 'any', key_combination))
+                        self.edges.add(
+                            (node, destination, "any", key_combination)
+                        )
 
         pprint(self.edges)
 
     def detect_edges_flags(self):
         self.deterministic = set()
         for node in self.nodes:
-            print('NODE', node)
+            print("NODE", node)
             transits = set(filter(lambda x: x[0] == node, self.edges))
             if len(transits) == 0:
                 continue
             if len(transits) == 1:
                 self.deterministic.add(transits.pop())
                 continue
-            if any(filter(lambda x: x[2] == 'any' or x[2] == 'eps' or x[2].isdigit(), transits)):
+            if any(
+                filter(
+                    lambda x: x[2] == "any" or x[2] == "eps" or x[2].isdigit(),
+                    transits,
+                )
+            ):
                 continue
             for alpha in self.alphabeth:
                 alpha_transits = set(filter(lambda x: x[2] == alpha, transits))
@@ -148,9 +176,46 @@ class MFA:
 
         self.readings = set(filter(lambda edge: edge[2].isdigit(), self.edges))
         for e in self.edges:
-            print(e, any(map(lambda x: x != 'c', e[3])))
+            print(e, any(map(lambda x: x != "c", e[3])))
             for x in e[3]:
-                print(x, 'c', x == 'c')
-        self.writings = set(filter(lambda edge: any(map(lambda x: x != 'c', edge[3])), self.edges))
-        self.memory_independend = set(filter(lambda edge: not edge[2].isdigit(), self.edges))
+                print(x, "c", x == "c")
+        self.writings = set(
+            filter(
+                lambda edge: any(map(lambda x: x != "c", edge[3])), self.edges
+            )
+        )
+        self.memory_independend = set(
+            filter(lambda edge: not edge[2].isdigit(), self.edges)
+        )
         pprint(self.deterministic)
+
+    def create_graph(self):
+        dot = graphviz.Digraph()
+
+        for node_id, label in self.lables.items():
+            dot.node(node_id, label)
+        for node_id in self.nodes:
+            dot.node(
+                node_id,
+                shape="doublecircle"
+                if node_id in self.finals
+                else "egg"
+                if node_id in self.inits
+                else "box"
+                if node_id in self.traps
+                else "circle",
+            )
+
+        for edge in self.edges:
+            color = "red" if edge in self.readings else "black"
+            color += ":blue" if edge in self.memory_independend else ":black"
+            color += ":green" if edge in self.writings else ":black"
+            color += ";0.33"
+            dot.edge(
+                edge[0],
+                edge[1],
+                label=", ".join([edge[2]] + list(edge[3])),
+                penwidth="3.5" if edge in self.deterministic else "2",
+                color=color,
+            )
+        dot.render("graph")
