9599e5666e38450f9a847d1f9b99be96fb0fde62
pug-coder <andrey.suchanov@yandex.ru>
2023-01-15 19:19:03 +0300

    main changes

--- FormalLanguagesTheory/lab5/main.py
+++ FormalLanguagesTheory/lab5/main.py
@@ -3,6 +3,13 @@ from parser import *
 
 import sys
 
+
+'''
+    Отразил в readme, но т.к. важно:
+    Для гарантированного выполнения запускать в терминале
+    python main.py 1
+    dot -Tsvg graph.dot > graph.svg
+'''
 if __name__ == '__main__':
     parsed, config = parse_mfa(sys.argv[1])
     print('PARSED')
