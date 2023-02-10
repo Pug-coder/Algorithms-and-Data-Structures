ebb16d22e1c235d09309a2e102e716bb1bd8f768
pug-coder <andrey.suchanov@yandex.ru>
2023-02-10 21:08:29 +0300

    remove f-string from main

--- FormalLanguagesTheory/lab2/main.py
+++ FormalLanguagesTheory/lab2/main.py
@@ -1,7 +1,7 @@
 from parser import CFG_Parser
 import sys
 
-parser_obj = CFG_Parser(f'tests/test1.txt')
+parser_obj = CFG_Parser('tests/test1.txt')
 grammar = parser_obj.parse_rules()
 
 g2 = grammar.lab_pipelene()
